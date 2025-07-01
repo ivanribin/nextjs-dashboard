afc67f131a0c87d8eb99a499731994e426eb09b1
Ivan Rybin <rybin1van@outlook.com>
2025-07-01 12:56:26 +0300

    feat: Accessibility, forms validation, eslint config

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,11 @@ import { revalidatePath } from "next/cache";
 import postgres from "postgres";
 import { redirect } from "next/navigation";
 
+export type State = {
+    errors?: IInvoiceErrors;
+    message?: string | null;
+};
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
@@ -17,12 +22,59 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+interface IInvoiceErrors {
+    customerId?: string;
+    amount?: string;
+    status?: string;
+}
+
+const invoiceErrorMessages: Record<keyof IInvoiceErrors, string> = {
+    customerId: "Please select a customer",
+    amount: "Please enter an amount greater that $0",
+    status: "Please select an invoice status",
+};
+
+const getInvoiceValidateErrorsByZodErrors = (
+    zodErrorsKeys: string[]
+): IInvoiceErrors => {
+    return zodErrorsKeys.reduce(
+        (invoiceValidateErrors: IInvoiceErrors, zodErrorKey: string) => {
+            const validateMessage: string =
+                invoiceErrorMessages[zodErrorKey as keyof IInvoiceErrors];
+
+            if (!Object.keys(invoiceValidateErrors).length) {
+                return {
+                    [zodErrorKey]: validateMessage,
+                };
+            }
+
+            return { ...invoiceValidateErrors, [zodErrorKey]: validateMessage };
+        },
+        {} as IInvoiceErrors
+    );
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get("customerId"),
         amount: formData.get("amount"),
         status: formData.get("status"),
     });
+
+    if (!validatedFields.success) {
+        const zodErrors: any = validatedFields.error.flatten().fieldErrors;
+
+        const validateErrors: IInvoiceErrors =
+            getInvoiceValidateErrorsByZodErrors(Object.keys(zodErrors));
+
+        return {
+            errors: validateErrors,
+            message: "Missing Fields. Failed to Create Invoice.",
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
+
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split("T")[0];
 
@@ -59,15 +111,13 @@ export async function updateInvoice(id: string, formData: FormData) {
     } catch (error) {
         // We'll log the error to the console for now
         console.error(error);
-    };
+    }
 
     revalidatePath("/dashboard/invoices");
     redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
-    
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
 }
